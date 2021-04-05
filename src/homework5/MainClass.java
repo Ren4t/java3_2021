30ce4bf44c47526cd977dfb7fa353ca939927f88
Ren4t <ren4tab@yandex.ru>
2021-04-05 15:48:25 +0300

    homework5_race

---
+++ src/homework5/MainClass.java
@@ -0,0 +1,26 @@
+package homework5;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
+public class MainClass {
+    public static final int CARS_COUNT = 4;
+    public static void main(String[] args) {
+        CyclicBarrier cb = new CyclicBarrier(CARS_COUNT);
+        Semaphore sm = new Semaphore(CARS_COUNT/2);
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(sm,cb,race, 20 + (int) (Math.random() * 10));
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+}
+
